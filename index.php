3f6e937078f199de34ba2410d5f71cc79cd354e7
ryanybrennan <ryanybrennan@msn.com>
2018-05-08 20:31:13 -0700

    added basic MVC layout regarding front end. Added routes.php, index.php, static_controller.php, templates, files in templates folder, static folder and files in static folder, and dynamic folder.

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+//require_once('connection.php');
+
+if (isset($_GET['controller']) && isset($_GET['action'])) {
+    $controller = $_GET['controller'];
+    $action     = $_GET['action'];
+} else {
+    $controller = 'static';
+    $action     = 'landing';
+}
+
+require_once('views/layout.php');
+?>
+
---
+++ routes.php
@@ -0,0 +1,30 @@
+<?php
+function call($controller, $action) {
+    require_once('controllers/'. $controller .'_controller.php');
+
+    switch($controller) {
+        case 'static':
+            $controller = new StaticController();
+            break;
+//        case 'books':
+//            require_once('model/model.php');
+//            $controller = new BooksController();
+//            break;
+    }
+
+    $controller->{ $action }();
+}
+
+$controllers = array('static' => ['landing', 'error']);
+
+if (array_key_exists($controller, $controllers)) {
+    if (in_array($action, $controllers[$controller])) {
+        call($controller, $action);
+    } else {
+        call('static', 'error');
+    }
+} else {
+    call('static', 'error');
+}
+
+?>
