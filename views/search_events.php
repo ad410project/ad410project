a954f9d4eba6167cf98db686ea71090ba1a4b4e7
HbugLubas <harry.lubas@bellevuecollege.edu>
2018-05-12 17:32:24 -0700

    initial commit

---
+++ views/search_events.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Harry
+ * Date: 5/8/2018
+ * Time: 3:55 PM
+ */
+?>
+
+
+<!DOCTYPE html>
+<html xmlns:float="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+
+    <style>
+        body {
+            background-color: snow;
+        }
+
+        #tabs_section {
+            float: left;
+            width: 40%;
+        }
+
+        #filter_section {
+            float: right;
+            margin-top: 12em;
+            margin-right: 8em;
+            alignment: center;
+            width: 35%;
+        }
+
+        #map {
+            width: 550px;
+            height: 400px;
+            background-color: grey;
+        }
+    </style>
+
+    <script>
+        function initMap() {
+            var seattle = {lat: 47.608013, lng: -122.335167};
+            var northSet = {lat: 47.699598, lng: -122.332292};
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 10,
+                center: seattle
+            });
+            var marker1 = new google.maps.Marker({
+                position: seattle,
+                map: map
+            });
+
+            var marker2 = new google.maps.Marker({
+                position: northSet,
+                map: map
+            })
+        }
+    </script>
+    <script async defer
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCf6m35Pbc-BmCZzxrlUlCWmMpHOrWyMms&callback=initMap">
+    </script>
+
+</head>
+<body>
+<div class="container">
+    <h3>Search Events</h3>
+    <br>
+    <section id="tabs_section">
+
+        <ul class="nav nav-tabs">
+
+            <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#map">Map</a></li>
+            <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#list">List</a></li>
+            <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#calendar">Calendar</a></li>
+        </ul>
+
+        <div class="tab-content">
+            <div id="map" class="container tab-pane active">
+
+            </div>
+
+
+            <div id="list" class="container tab-pane fade">
+                <ul>Activity Name, Address, Description, Contact</ul>
+                <ul>Activity Name, Address, Description, Contact</ul>
+                <ul>Activity Name, Address, Description, Contact</ul>
+                <ul>Activity Name, Address, Description, Contact</ul>
+                <ul>Activity Name, Address, Description, Contact</ul>
+                <ul>Activity Name, Address, Description, Contact</ul>
+            </div>
+
+            <div id="calendar" class="container tab-pane fade">
+                <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;height=400&amp;wkst=1&amp;
+                bgcolor=%23FFFFFF&amp;src=n2arhrlms9u36kgjs3sb8c2ago%40group.calendar.google.com&amp;color=%23AB8B00&amp;ctz=America%2FLos_Angeles"
+                        style="border-width:0" width="500" height="400" frameborder="0" scrolling="no"></iframe>
+            </div>
+    </section>
+    <section id="filter_section">
+
+        <div class="col-lg-12">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Search for...">
+                <span class="input-group-btn">
+                <button class="btn btn-secondary" type="button">Go!</button>
+                </span>
+                <div class="dropdown">
+                    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                        Filter
+                    </button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Filter 1</a>
+                        <a class="dropdown-item" href="#">Filter 2</a>
+                        <a class="dropdown-item" href="#">Filter 3</a>
+                        <a class="dropdown-item" href="#">Filter 4</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </section>
+</div>
+
+</body>
+</html>
+
