5e31c5eb9dc3a705bb54c4720217ace6b75c30c1
Yahya Hussein <yahyahussein91@gmail.com>
2018-05-20 14:47:17 -0700

    made changes to user.php

---
+++ models/users.php
@@ -0,0 +1,60 @@
+<?php 
+class  user{
+	public $userId;
+	public $firstName;
+	public $lastName;
+	public $email;
+	public $password;
+	public $phoneNumber;
+	public $notificationState;
+	public $userTypeId;
+
+}
+
+	public function __construct($userId, $firstName, $lastName, $email, $password, $phoneNumber, $notificationState, $userTypeId){
+
+		$this->userId 					= $userId;
+	    $this->firstName 				= $firstName;
+	    $this->lastName 				= $lastName;
+	    $this->email					= $email;
+	    $this->password					= $password;
+	    $this->phoneNumber  			= $phoneNumbe;
+	    $this->notificationState 		= $notificationState;
+		$this->userTypeId				= $userTypeId;
+	}
+
+	public function addUser($userId, $firstName, $lastName, $email, $password, $phoneNumber, $notificationState, $userTypeId){
+		//add user to database
+		$req = $db->prepare('INSERT INTO users firstName, lastName, email, password, phoneNumber, notificationState, userTypeId 
+                                                       VALUES ?, ?, ?, ?, ?, ?, ?)';  
+
+        $req->bind_param($userId, $firstName, $lastName, $email, $password, $phoneNumber, $notificationState, $userTypeId);
+        $req->execute();                                               
+	}
+
+	public static function editUser($userId, $firstName, $lastName, $email, $password, $phoneNumber, $notificationState, $userTypeId){
+		//edit user 
+        $req = $db->prepare('UPDATE TABLE_NAME SET 
+                firstName = ?,
+                lastName = ?,
+                email = ?,
+                password = ?,
+                phoneNumber = ?,
+                notificationState = ?,
+                userTypeId = ? WHERE id = ?');
+
+        $req->bind_param('$userId, $firstName, $lastName, $email, $password, $phoneNumber, $notificationState, $userTypeId');
+        $req->execute();
+	}
+
+	public static function deleteUser($user_id){
+		$query = 'DELETE FROM users WHERE user_id = ?';
+		return ($this->db->query($query));
+	}
+
+	public static function getUserById($user_id){
+		// Prepare the query 
+        $req = $db->prepare('SELECT column_name FROM table_name WHERE id = ?');
+		return ($this->db->query($query));
+	}
+?>
