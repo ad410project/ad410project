5ffba74cf5ca49bde1183884c82d195a0344d868
gshields5541 <gshields5541@gmail.com>
2018-05-28 20:39:07 -0700

    Merge branch 'Development' of https://github.com/ad410project/ad410project into CSS
    # Conflicts:
    #	models/AD410_DB_Model.mwb
    #	models/README.txt
    
    Also attempted to address relative link issues in header.php, landing.php and layout.php. Reverted events.php back to original file due to bad merge with incorrectly named "development" branch.
     Adjusted the width of map component in styles.css to 1105px in order to encompass the width of search events navbar

--- models/event.php
+++ models/event.php
@@ -1,13 +1,11 @@
 <?php
 class event
 {
-
     private $eventId;
     private $organizationId;
-    private $creatorId;
     private $name;
     private $description;
-    private $location;
+    private $address;
     private $startDate;
     private $endDate;
     private $categories;
@@ -17,16 +15,14 @@ class event
     private $maxAge;
     private $registrationOpen;
     private $registrationClose;
-
-    public function __construct($eventId, $creatorId, $name, $description, $location, $startDate, $endDate,
+    public function __construct($eventId, $name, $description, $address, $startDate, $endDate,
                                 $organizationId, $categories, $type, $price, $minAge, $maxAge,
                                 $registrationOpen, $registrationClose)
     {
         $this->eventId = $eventId;
-        $this->creatorId = $creatorId;
         $this->name = $name;
         $this->description = $description;
-        $this->location = $location;
+        $this->address = $address;
         $this->startDate = $startDate;
         $this->endDate = $endDate;
         $this->organizationId = $organizationId;
@@ -38,174 +34,230 @@ class event
         $this->registrationOpen = $registrationOpen;
         $this->registrationClose = $registrationClose;
     }
-
     public static function addEvent($event)
     {
+        //get instance of db
+        $db = Db::getInstance();
         //add event to database
     }
-
     public static function deleteEvent($eventId)
     {
+        //get instance of db
+        $db = Db::getInstance();
         //delete the event
     }
-
+    public static function getAllEvents()
+    {
+        //get instance of db
+        $db = Db::getInstance();
+        $result = $db->query('CALL getEvents()');
+        $db->close();
+        return $result;
+    }
+    public static function getEventById($eventId)
+    {
+        //get instance of db
+        $db = Db::getInstance();
+        //bind the in parameters
+        $stmt = $db->prepare("SET @myEventId = ?");
+        $stmt->bind_param('i', $eventId);
+        $stmt->execute();
+        //execute the procedure
+        $result = $db->query('CALL getEvent(@myEventId)');
+        $db->close();
+        return $result;
+    }
     public static function getEventsByUserId($userId)
     {
+        //get instance of db
+        $db = Db::getInstance();
         //return events for the user ID
     }
-
+    public static function getEventsByChildId($childId)
+    {
+        //get instance of db
+        $db = Db::getInstance();
+        //bind the in parameters
+        $stmt = $db->prepare("SET @myChildId = ?");
+        $stmt->bind_param('i', $childId);
+        $stmt->execute();
+        //execute the procedure
+        $result = $db->query('CALL getChildEvents(@myChildId)');
+        $db->close();
+        return $result;
+    }
     public static function getEventsByCategory($category)
     {
-        //return events matching category
+        //get instance of db
+        $db = Db::getInstance();
+        //bind the in parameters
+        $stmt = $db->prepare("SET @myCategoryId = ?");
+        $stmt->bind_param('i', $category);
+        $stmt->execute();
+        //execute the procedure
+        $result = $db->query('CALL getCategoryEvent(@myCategoryId)');
+        $db->close();
+        return $result;
     }
-
+    //return events matching type
     public static function getEventsByType($type)
     {
-        //return events matching type
+        //get instance of db
+        $db = Db::getInstance();
+        //bind the in parameters
+        $stmt = $db->prepare("SET @myTypeId = ?");
+        $stmt->bind_param('i', $type);
+        $stmt->execute();
+        //execute the procedure
+        $result = $db->query('CALL getTypeEvent(@myTypeId)');
+        $db->close();
+        return $result;
     }
-
     public static function getEventsByKeyword($keyword)
     {
+        //get instance of db
+        $db = Db::getInstance();
         //return events with keyword in name or description
     }
-
     public static function getEventsByDateRange($startDate, $endDate)
     {
-        //return events that fall within the date range
+        //get instance of db
+        $db = Db::getInstance();
+        //bind the in parameters
+        $stmt = $db->prepare("SET @startTime = ?");
+        $stmt->bind_param('i', $startDate);
+        $stmt->execute();
+        $stmt = $db->prepare("SET @endTime = ?");
+        $stmt->bind_param('i', $endDate);
+        $stmt->execute();
+        //execute the procedure
+        $result = $db->query('CALL getEventsInDateRange(@startTime, @endTime)');
+        $db->close();
+        return $result;
     }
-
     public static function getEventsByAgeRange($minAge, $maxAge)
     {
+        //get instance of db
+        $db = Db::getInstance();
         //return events for the age range
     }
-
     public static function getEventsByPriceRange($minPrice, $maxPrice)
     {
+        //get instance of db
+        $db = Db::getInstance();
         //return events for the price range
     }
-
     public static function getEventsByLocation($location, $distance)
     {
+        //get instance of db
+        $db = Db::getInstance();
         //return events within a range of the location
     }
-
     public static function editEventField($eventId, $fieldToChange, $changeToThis)
     {
+        //get instance of db
+        $db = Db::getInstance();
         //edit event characteristics, might replace with multiple methods in the
         //form of setEventName, setEventDescription, setEventLocation, etc.
     }
-
     public static function addCategory($eventId, $category)
     {
+        //get instance of db
+        $db = Db::getInstance();
         //add a category to the event in the database
-
         //categories, types, and addresses likely need their own
         //methods for changes since they can have multiple entries
     }
-
     public static function removeCategory($eventId, $category)
     {
+        //get instance of db
+        $db = Db::getInstance();
         //remove a category from the event in the database
-
         //categories, types, and addresses likely need their own
         //methods for changes since they can have multiple entries
     }
-
     public static function addType($eventId, $type)
     {
+        //get instance of db
+        $db = Db::getInstance();
         //add a type to the event in the database
-
         //categories, types, and addresses likely need their own
         //methods for changes since they can have multiple entries
     }
-
     public static function removeType($eventId, $type)
     {
+        //get instance of db
+        $db = Db::getInstance();
         //remove a type from the event in the database
-
         //categories, types, and addresses likely need their own
         //methods for changes since they can have multiple entries
     }
-
     public static function addAddress($eventId, $address)
     {
+        //get instance of db
+        $db = Db::getInstance();
         //add an address to the event in the database
-
         //categories, types, and addresses likely need their own
         //methods for changes since they can have multiple entries
     }
-
     public static function removeAddress($eventId, $address)
     {
+        //get instance of db
+        $db = Db::getInstance();
         //remove an address from the event in the database
-
         //categories, types, and addresses likely need their own
         //methods for changes since they can have multiple entries
     }
-
     public function getEventId()
     {
         return $this->eventId;
     }
-
     public function getName()
     {
         return $this->name;
     }
-
     public function getDescription()
     {
         return $this->description;
     }
-
-    public function getLocation()
+    public function getAddress()
     {
-        return $this->location;
+        return $this->address;
     }
-
     public function getStartDate(){
         return $this->startDate;
     }
-
     public function getEndDate(){
         return $this->endDate;
     }
-
     public function getOrganizationId()
     {
         return $this->organizationId;
     }
-
     public function getCategories()
     {
         return $this->categories;
     }
-
     public function getType()
     {
         return $this->type;
     }
-
     public function getPrice()
     {
         return $this->price;
     }
-
     public function getMinAge()
     {
         return $this->minAge;
     }
-
     public function getMaxAge()
     {
         return $this->maxAge;
     }
-
     public function getRegistrationOpen()
     {
         return $this->registrationOpen;
     }
-
     public function getRegistrationClose()
     {
         return $this->registrationClose;
