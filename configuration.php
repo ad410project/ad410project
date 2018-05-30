3abdf170d88f137b69b63fd3e9c0478db36dbd52
mtolrom <SDE2013@outlook.com>
2018-05-30 10:20:38 -0700

    Removing test lines in configuration.php

--- configuration.php
+++ configuration.php
@@ -4,17 +4,11 @@
  * User: luv.sharma
  * Date: 2/19/18
  * Time: 4:42 PM
- *
+ */
 define ('DB_USER', "root");
 define ('DB_PASSWORD', "root");
 define ('DB_DATABASE', "mydb");
 define ('DB_HOST', "localhost");
 define ('DB_PORT', 3306);
- * 
- */
 
-define ('DB_USER', "practicum");
-define ('DB_PASSWORD', "NetiMeki@99");
-define ('DB_DATABASE', "WebPracticumDB2");
-define ('DB_HOST', "localhost");
-define ('DB_PORT', 3306);
+
