65ba3b806c2f6d101c817902c7f5f8587c38ce25
gshields5541 <gshields5541@gmail.com>
2018-06-02 13:18:26 -0700

    1. Removed the Search Events button on the top right of the nav bar.  2. Changed the .btn-transparent:hover 'background-color setting' to #2D6077 so that the "Search Events" button would be more readable when the cursor hovers over it.

--- views/static/login.php
+++ views/static/login.php
@@ -3,16 +3,16 @@
         <img id="avatar" src="views/images/img_avatar2.png"/>
         <h1>Log In</h1>
     </div>
-<form class="form-signin" method="POST">
-    <div class="container" id="container">
-        <label for="uname"><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="email" required>
+    <form class="form-signin" method="POST">
+        <div class="container" id="container">
+            <label for="uname"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="email" required>
 
-        <label for="psw"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="password" required>
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="password" required>
 
-        <button class="btn btn-lg btn-primary btn-block" type="submit" name="login_btn">Login</button>
-    </div>
-</form>
+            <button class="btn btn-lg btn-primary btn-block" type="submit" name="login_btn">Login</button>
+        </div>
+    </form>
 </div>
 
