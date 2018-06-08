4fb40e6174faf7294e06440ad3a534d61055be79
ad410project <39012557+ad410project@users.noreply.github.com>
2018-06-08 04:22:45 -0700

    Update layout.php

--- views/layout.php
+++ views/layout.php
@@ -12,6 +12,7 @@
             integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
             integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+       <script src="views/scripts/main.js"></script>
 
 <!--    CSS Section-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" id="bootstrap-css"
