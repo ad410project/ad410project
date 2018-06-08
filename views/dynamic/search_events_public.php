9f23b9f831c8ab42dbfa2251e07251535a435b33
Tet Contado <delrio.tet@gmail.com>
2018-06-08 01:10:36 -0700

    removed commented blocks

--- views/dynamic/search_events_public.php
+++ views/dynamic/search_events_public.php
@@ -7,31 +7,7 @@
  */
 
 ?>
-<!--<!DOCTYPE html>-->
-<!--<html lang="en">-->
-<!--<head>-->
-<!--       <meta charset="utf-8">-->
-<!--    <link rel='stylesheet prefetch'-->
-<!--          href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css'>-->
-<!--    <link rel='stylesheet prefetch'-->
-<!--          href='https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css'>-->
-<!--    <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.theme.min.css'>-->
-<!--    <link rel='stylesheet prefetch'-->
-<!--          href='https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.transitions.min.css'>-->
-<!---->
-<!--    <link rel="stylesheet" href="../styles/styles.css">-->
-<!---->
-<!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">-->
-<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>-->
-<!--    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>-->
-<!--    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>-->
-<!---->
-<!--    <script src='https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js'></script>-->
-<!--<!--    <script src="../scripts/main.js"></script>-->-->
-<!---->
-<!---->
-<!--</head>-->
-<!--<body id="events_body">-->
+
 <div class="container" style="margin-top: 50px">
     <center><h2>Public Events</h2></center>
 
